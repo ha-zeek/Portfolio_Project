83da3d90434b1ef5307ac5bc5529bb902d066d7f
ha-zeek <haziq8898@gmail.com>
2023-03-07 00:26:50 +0800

    homepage completed

---
+++ portfolio-1/src/components/Home/index.js
@@ -0,0 +1,44 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import AnimatedLetters from "../AnimatedLetters";
+import "./index.scss";
+
+const Home = () => {
+  const [letterClass, setletterClass] = useState("text-animate");
+
+  useEffect(() => {
+    setTimeout(() => {
+      setletterClass("text-animate-hover");
+    }, 4000);
+  }, []);
+
+  return (
+    <div className="container home-page">
+      <div className="text-zone">
+        <h1>
+          <span className={letterClass}>H</span>
+          <span className={`${letterClass} _12`}>i,</span>
+          <br />
+
+          <AnimatedLetters
+            letterClass={letterClass}
+            strArray={"I'm Haziq Luqman".split("")}
+            idx={15}
+          />
+          <AnimatedLetters
+            letterClass={letterClass}
+            strArray={"Web Developer.".split("")}
+            idx={20}
+          />
+        </h1>
+        <br />
+        <h2>Frontend Developer | Project Management</h2>
+        <Link to="/contact" className="flat-button">
+          CONTACT ME
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
---
+++ portfolio-1/src/components/Sidebar/index.js
@@ -0,0 +1,57 @@
+import './index.scss';
+import { Link, NavLink } from 'react-router-dom';
+import LogoHZ from '../../assets/images/logo-hz.jpg';
+import LogoHazeek from '../../assets/images/logo-hazeek.jpg';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons';
+import { faGithubSquare, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+
+const Sidebar = () => {
+  return (
+    <div className="nav-bar">
+      <Link className="logo" to="/">
+        <img src={LogoHZ} alt="logo" />
+        <img className="sub-logo" src={LogoHazeek} alt="hazeek" />
+      </Link>
+      <nav>
+        <NavLink exact="true" activeclassname="active" to="/">
+          <FontAwesomeIcon icon={faHome} color="white" />
+        </NavLink>
+        <NavLink
+          exact="true"
+          activeclassname="active"
+          className="about-link"
+          to="/about"
+        >
+          <FontAwesomeIcon icon={faUser} color="white" />
+        </NavLink>
+        <NavLink
+          exact="true"
+          activeclassname="active"
+          className="contact-link"
+          to="/contact"
+        >
+          <FontAwesomeIcon icon={faEnvelope} color="white" />
+        </NavLink>
+      </nav>
+      <ul>
+        <li>
+          <a
+            target="_blank"
+            rel="norefferer"
+            href="https://www.linkedin.com/in/haziq-luqman-611500172/"
+          >
+            <FontAwesomeIcon icon={faLinkedin} color="white" />
+          </a>
+        </li>
+        <li>
+          <a target="_blank" rel="norefferer" href="https://github.com/ha-zeek">
+            <FontAwesomeIcon icon={faGithubSquare} color="white" />
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Sidebar;
